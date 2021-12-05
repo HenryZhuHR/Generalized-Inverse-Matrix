4adf2ad64017b75cbadcf382b2646895283c51b9
Henry Zhu <38650110+HenryZhuHR@users.noreply.github.com>
2021-12-05 18:01:20 +0800

    matrix

--- src/execsize__6_4_6.m
+++ src/execsize__6_4_6.m
@@ -1,44 +1,79 @@
-clear all;
-clc;
-%% 6.4 - 6.
+clear all;clc;
+
+%% A
 A = [
     1, 0, 0, 1;
     1, 1, 0, 0;
     0, 1, 1, 0;
     0, 0, 1, 1
     ];
-A_inverse=pinv(A);
-
 disp("Matrix A:")
 disp(A)
-disp("Generalized Inverse Matrix of Matrix A:")
-disp(A_inverse)
 
+%% compute Generalized Inverse Matrix
+B=pinv(A);
+disp("Generalized Inverse Matrix of Matrix A:")
+disp(B)
+
+disp("Penrose condition 1:")
+disp("(1) AXA=A")
+disp(A*B*A)
+disp(A)
+
+disp("Penrose condition 2:")
+disp("(2) XAX=X")
+disp(B*A*B)
+disp(B)
+
+disp("Penrose condition 3:")
+disp("(3) (AX)^H=AX")
+disp((A*B)')
+disp(A*B)
+
+disp("Penrose condition 4:")
+disp("(4) (XA)^H=XA")
+disp((B*A)')
+disp(B*A)
 
 %% (1) b=[1;1;1;1];
 disp("=================================")
 b=[1;1;1;1];
-x_0=A_inverse*b;
 disp("for b=")
 disp(b)
-disp("compute A*A^+*b=")
-disp(A*A_inverse*b)
+
+x_0=B*b;
+disp("x_0=")
+disp(x_0)
 disp("compute A*x_0=")
 disp(A*x_0)
-
+disp("compute A*A^+*b=")
+disp(A*B*b)
 disp("A*A^+*b = A*x_0 = b, so Ax=b is a consistent linear system, so x_0 = ")
 disp(x_0)
+disp("\min\limits_x ||x||")
+disp(norm(x_0))
+
+disp("compute A^+*b=")
+disp(B*b)
+disp("compute I-A^+*A=")
+disp(eye(4)-B*A)
+
 
 %% (2) b=[1;0;1;0];
 disp("=================================")
 b=[1;0;1;0];
-x_0=A_inverse*b;
 disp("for b=")
 disp(b)
-disp("compute A*A^+*b=")
-disp(A*A_inverse*b)
+
+x_0=B*b;
+disp("x_0=")
+disp(x_0)
 disp("compute A*x_0=")
 disp(A*x_0)
-
-disp("A*A^+*b = A*x_0 \neq b, so Ax=b is an inconsistent linear system, so x_0 = ")
+disp("compute A*A^+*b=")
+disp(A*B*b)
+disp("A*A^+*b = A*x_0 = b, so Ax=b is a consistent linear system, so x_0 = ")
 disp(x_0)
+
+disp("\min\limits_x ||Ax-b||")
+disp(norm(A*x_0-b))
